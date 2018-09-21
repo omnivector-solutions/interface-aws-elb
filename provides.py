f75f20bfdd75cf3f59130bf6cf773cd1da9a36d6
jamesbeedy <jamesbeedy@gmail.com>
2018-09-21 00:10:47 -0700

    add departed hook to requires

--- provides.py
+++ provides.py
@@ -3,12 +3,14 @@ from charms.reactive import Endpoint
 
 class AwsElbProvides(Endpoint):
 
-    def configure(self, instance_id, instance_region, instance_port):
+    def configure(self, instance_id, instance_region,
+                  instance_port, health_check_endpoint):
         """
         Configure the aws-elb relation by providing:
             - instance_id
             - instance_region
             - instance_port
+            - health_check_endpoint
         """
 
         for relation in self.relations:
@@ -16,4 +18,5 @@ class AwsElbProvides(Endpoint):
                 'instance_id': instance_id,
                 'instance_region': instance_region,
                 'instance_port': instance_port,
+                'health_check_endpoint': health_check_endpoint
             })
--- requires.py
+++ requires.py
@@ -1,5 +1,5 @@
-from charms.reactive import when
-from charms.reactive import set_flag
+from charms.reactive import when, when_not
+from charms.reactive import clear_flag, set_flag
 from charms.reactive import Endpoint
 
 
@@ -11,21 +11,17 @@ class AwsElbRequires(Endpoint):
                for unit in self.all_joined_units):
             set_flag(self.expand_name('available'))
 
+    @when_not('endpoint.{endpoint_name}.joined')
+    def departed(self):
+        clear_flag(self.expand_name('available'))
+
+
     def list_unit_data(self):
         """
         Get the list of the relation info for each unit.
 
-        Returns a list of dicts, where each dict contains the elasticsearch
-        cluster name, the host (address)
-        and the port (as a string), as well as
-        the relation ID and remote unit name that provided the site.
-
-        For example::
-            [
-                {
-                    'port': '80',
-                },
-            ]
+        Returns a list of dicts, where each dict contains the instance_id,
+        instance_region, instance_port, and health_check_endpoint.
         """
         units_data = []
         for relation in self.relations:
@@ -33,11 +29,14 @@ class AwsElbRequires(Endpoint):
                 instance_id = unit.received['instance_id']
                 instance_region = unit.received['instance_region']
                 instance_port = unit.received['instance_port']
-                if not (instance_id and instance_region and instance_port):
+                health_check_endpoint = unit.received['health_check_endpoint']
+                if not (instance_id and instance_region and
+                        instance_port and health_check_endpoint):
                     continue
                 units_data.append({
                     'instance_id': instance_id,
                     'instance_region': instance_region,
                     'instance_port': instance_port,
+                    'health_check_endpoint': health_check_endpoint,
                 })
         return units_data
